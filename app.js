d0e2af111b14a03046611819e818d2b309dea09f
Alexandre Angrignon <alex040892@gmail.com>
2024-03-08 23:08:25 +1300

    Dleete and Mark

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const todoList = document.querySelector(".todo-list");
 
 //Event Listeners
 todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
 
 //Functions
 function addTodo(event) {
@@ -33,4 +34,20 @@ function addTodo(event) {
   todoList.appendChild(todoDiv);
   // Clear Todo INPUT VALUE
   todoInput.value = "";
+}
+
+function deleteCheck(e) {
+  console.log(e.target);
+  const item = e.target;
+  //DELETE TODO
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    todo.remove();
+  }
+
+  //CHECK MARK
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
 }
